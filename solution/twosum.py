099ef2cca78a1620f1c71f8d44548d817a79aede
zhangda <dazhang@lenovo.com>
2015-06-25 16:46:09 +0800

    init project

---
+++ solution/twosum.py
@@ -0,0 +1,15 @@
+'''
+Created on 2015/6/25
+
+@author: dazhang
+'''
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer[]}
+    def twoSum(self, nums, target):
+        pass
+
+if __name__ == '__main__':
+    pass
